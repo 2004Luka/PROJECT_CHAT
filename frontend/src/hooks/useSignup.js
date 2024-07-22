10434e4dfa1dc11db0c6b68b99f3dae3ceb458a9
2004Luka <luka.chakhunashvili1@gmail.com>
2024-07-23 01:10:11 +0400

    added authContext

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,31 +1,44 @@
 import React, { useState } from 'react'
 import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
 const useSignup = () => {
-  const [loading,setLoading] = useState(false);
+    const [loading,setLoading] = useState(false);
+    const [authUser,setAuthUser] = useAuthContext();
 
-  const signup = async({fullName,username,password,confirmPassword,gender})=>{
-    const success= handleInputErrors({fullName,username,password,confirmPassword,gender})
-    if(!success) return;
 
-    setLoading(true);
-    try{
-        const res = await fetch("/api/auth/signup",{
-            method:"POST",
-            headers:{"Content-Type":"application/json"},
-            body:JSON.stringify({fullName,username,password,confirmPassword,gender})
-        });
 
-        const data = await res.json();
-        console.log(data);
-        toast.success("sign up")
-    }catch(error){
-        toast.error(error.message)
-    }finally{
-        setLoading(false);
+    const signup = async({fullName,username,password,confirmPassword,gender})=>{
+        const success= handleInputErrors({fullName,username,password,confirmPassword,gender})
+        if(!success) return;
+
+        setLoading(true);
+        try{
+            const res = await fetch("/api/auth/signup",{
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify({fullName,username,password,confirmPassword,gender})
+            });
+
+            const data = await res.json();
+            
+            if(data.error){
+                throw new Error(data.error)
+            }
+            //localstorage
+            localStorage.setItem("caht-user",JSON.stringify(data));
+            //context
+            setAuthUser(data);
+
+            console.log(data);
+            toast.success("sign up")
+        }catch(error){
+            toast.error(error.message)
+        }finally{
+            setLoading(false);
+        }
     }
-  }
 
-  return {loading,signup}
+    return {loading,signup}
 }
 
 export default useSignup
