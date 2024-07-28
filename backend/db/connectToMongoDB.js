8f2822980509a7dd50306d09c9113104fcaa3f04
2004Luka <116211653+2004Luka@users.noreply.github.com>
2024-07-28 14:17:33 +0400

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const connectToMongoDB= async () =>{
+const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URL);
+        await mongoose.connect(process.env.MONGO_DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 10000, // Adjust the timeout as needed
+        });
         console.log("Connected to MongoDB");
-    }catch(eror){
-        console.log("error connecting to mongo db")
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
     }
-}
-
+};
 
 export default connectToMongoDB;
